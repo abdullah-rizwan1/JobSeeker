31a0e65a6810d69548b2746684bdd2b33da82bf8
Abdullah Rizwan <abdullahrn8@gmail.com>
2024-04-14 23:27:51 +0500

    Added missing files

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <div className="about">
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sunt alias id officiis labore deleniti aliquam atque unde non odit ipsum mollitia neque magnam, numquam est ratione excepturi laboriosam. Sapiente!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, fugiat consectetur facilis officiis earum error numquam, quasi maxime asperiores mollitia illum, velit quis repellendus. Corporis nihil id blanditiis provident ipsa.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit ullam voluptas voluptates aut omnis alias aspernatur labore neque facilis, esse earum iure consectetur consequatur deserunt, sint excepturi odit blanditiis laboriosam.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Welcome</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sunt alias id officiis labore deleniti aliquam atque unde non odit ipsum mollitia neque magnam, numquam est ratione excepturi laboriosam. Sapiente!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, fugiat consectetur facilis officiis earum error numquam, quasi maxime asperiores mollitia illum, velit quis repellendus. Corporis nihil id blanditiis provident ipsa.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit ullam voluptas voluptates aut omnis alias aspernatur labore neque facilis, esse earum iure consectetur consequatur deserunt, sint excepturi odit blanditiis laboriosam.</p>
+        </div>
+    )
+}
+
+export default Home
